5b2c6e37fdd7ff6c426d38bde53cd0a62dd059d3
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2023-02-26 18:11:43 +0100

    Redesign index page

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ let AIScore = 0;
 
 window.onload = initializeGame();
 
-function generateAIPick() {
-	let AIchoice = Math.floor(Math.random() * 3);
+function initializeGame() {
+	const buttonPanel = document.querySelector(".button-panel");
 
-	switch (AIchoice) {
-		case 0:
-			return "rock";
+	buttonPanel.childNodes.forEach((button) => {
+		button.addEventListener("click", () => {
+			playRound(button.textContent.toLowerCase());
+		});
+	});
 
-		case 1:
-			return "paper";
+	const playAgainButton = document.querySelector(".play-again");
+	playAgainButton.addEventListener("click", () => {
+		location.reload();
+	});
 
-		case 2:
-			return "scissors";
-	}
+	changePlayAgainButtonVisibility();
 }
 
 function playRound(playerChoice) {
@@ -33,16 +35,17 @@ function playRound(playerChoice) {
 	if (playerChoice === AIchoice) {
 		message = "It's a draw!";
 	} else if (playerChoice === "rock" && AIchoice === "scissors") {
-		message = "Congrats! You won the game!";
+		message = "Congrats! This round is yours to take!";
 		playerScore++;
 	} else if (playerChoice === "paper" && AIchoice === "rock") {
-		message = "Congrats! You won the game!";
+		message = "Congrats! This round is yours to take!";
 		playerScore++;
 	} else if (playerChoice === "scissors" && AIchoice === "paper") {
-		message = "Congrats! You won the game!";
+		message = "Congrats! This round is yours to take!";
 		playerScore++;
 	} else {
-		message = "You lost the game! May luck be on your side next time!";
+		message =
+			"You lost that round, but it's not over yet! Get ready for the next one!";
 		AIScore++;
 	}
 
@@ -55,10 +58,25 @@ function playRound(playerChoice) {
 		AIScore
 	);
 
-	printRoundInfo(roundInfo);
+	displayRoundInfo(roundInfo);
 
 	if (playerScore === 5 || AIScore === 5) {
 		checkGameOver();
+	}
+}
+
+function generateAIPick() {
+	let AIchoice = Math.floor(Math.random() * 3);
+
+	switch (AIchoice) {
+		case 0:
+			return "rock";
+
+		case 1:
+			return "paper";
+
+		case 2:
+			return "scissors";
 	}
 }
 
@@ -73,11 +91,33 @@ function checkGameOver() {
 }
 
 function EndGame() {
-	const buttons = document.querySelectorAll("button");
-	buttons.forEach((button) => {
+	const buttonPanel = document.querySelector(".button-panel");
+	buttonPanel.childNodes.forEach((button) => {
 		button.disabled = true;
 	});
+
+	changePlayAgainButtonVisibility();
 }
+
+function changePlayAgainButtonVisibility() {
+	const playAgainButton = document.querySelector(".play-again");
+	if (playAgainButton.style.display === "none") {
+		playAgainButton.style.display = "block";
+		playAgainButton.innerHTML = "Play Again";
+	} else {
+		playAgainButton.style.display = "none";
+	}
+}
+
+function displayRoundInfo(roundOutcome) {
+	let formattedRoundOutcome = formatRoundInfo(roundOutcome);
+
+	displayRoundNumber(formattedRoundOutcome);
+	displayScores(formattedRoundOutcome);
+	displayWeaponChoices(formattedRoundOutcome);
+	displayRoundStatus(formattedRoundOutcome);
+}
+
 function formatRoundInfo(roundOutcome) {
 	for (let i = 0; i < roundOutcome.length + 1; i++) {
 		switch (i) {
@@ -88,7 +128,7 @@ function formatRoundInfo(roundOutcome) {
 			case 1:
 				roundOutcome[1] =
 					roundOutcome[1].charAt(0).toUpperCase() + roundOutcome[1].slice(1);
-				roundOutcome[1] = `Player's Choice: ${roundOutcome[1]}`;
+				roundOutcome[1] = `Player Choice: ${roundOutcome[1]}`;
 				break;
 			case 2:
 				roundOutcome[2] =
@@ -96,7 +136,7 @@ function formatRoundInfo(roundOutcome) {
 				roundOutcome[2] = `AI Choice: ${roundOutcome[2]}`;
 				break;
 			case 4:
-				roundOutcome[4] = `Player's Score: ${roundOutcome[4]}`;
+				roundOutcome[4] = `Player Score: ${roundOutcome[4]}`;
 				break;
 			case 5:
 				roundOutcome[5] = `AI Score: ${roundOutcome[5]}`;
@@ -107,17 +147,26 @@ function formatRoundInfo(roundOutcome) {
 	return roundOutcome;
 }
 
-function printRoundInfo(roundOutcome) {
-	let formattedRoundOutcome = formatRoundInfo(roundOutcome);
-	alert(formattedRoundOutcome.join("\n"));
+function displayRoundNumber(roundOutcome) {
+	const roundParagraph = document.querySelector(".round-number");
+	roundParagraph.innerText = roundOutcome[0];
 }
 
-function initializeGame() {
-	const buttons = document.querySelectorAll("button");
+function displayScores(roundOutcome) {
+	const playerScoreParagraph = document.querySelector(".player-score");
+	playerScoreParagraph.innerText = roundOutcome[4];
+	const aiScoreParagraph = document.querySelector(".ai-score");
+	aiScoreParagraph.innerText = roundOutcome[5];
+}
 
-	buttons.forEach((button) => {
-		button.addEventListener("click", () => {
-			playRound(button.textContent.toLowerCase());
-		});
-	});
+function displayWeaponChoices(roundOutcome) {
+	const playerChoice = document.querySelector(".player-choice");
+	playerChoice.innerText = roundOutcome[1];
+	const aiChoice = document.querySelector(".ai-choice");
+	aiChoice.innerText = roundOutcome[2];
+}
+
+function displayRoundStatus(roundOutcome) {
+	const roundStatus = document.querySelector(".round-status");
+	roundStatus.innerText = roundOutcome[3];
 }
